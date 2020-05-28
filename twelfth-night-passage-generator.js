9e3d0b4ffcd6eaa1806303357406ff68bb4cbc35
Zhulander <64768780+Zhulander@users.noreply.github.com>
2020-05-28 21:07:50 +0800

    Basic Algorithm

--- twelfth-night-passage-generator.js
+++ twelfth-night-passage-generator.js
@@ -8,12 +8,26 @@ var randomlineend = randomlinestart + 40;
 
 // Act 1//
 
-function generatepassage(randomact, randomscene, randomlinestart, randomlineend) {
+function generatepassage(randomact, randomscene, randomlinestart, randomlineend, randomact2, randomscene2, randomlinestart2, randomlineend2, randomact3, randomscene3, randomlinestart3, randomlineend3) {
  
-var randomact = Math.floor(Math.random()*5+1);
+var randomact = Math.floor(Math.random()*2+1);
 var randomscene = Math.floor(Math.random()*4+1);
 var randomlinestart = Math.floor(Math.random()*200+1);
 var randomlineend = randomlinestart + 40;
+
+var randomact2 = Math.floor(Math.random()*3+2);
+var randomscene2 = Math.floor(Math.random()*4+1);
+var randomlinestart2 = Math.floor(Math.random()*200+1);
+var randomlineend2 = randomlinestart2 + 40;
+
+var randomact3 = Math.floor(Math.random()*2+4);
+var randomscene3 = Math.floor(Math.random()*4+1);
+var randomlinestart3 = Math.floor(Math.random()*200+1);
+var randomlineend3 = randomlinestart3 + 40;
+
+
+
+//  passage 1 //
 
 if (randomact == 1) {
    randomscene = Math.floor(Math.random()*5+1)
@@ -112,11 +126,226 @@ if (randomact == 5) {
    randomscene = 1
    randomlinestart = Math.floor(Math.random()*345+1)
    randomlineend = randomlinestart + 40;
+
+
 };
 
+//  passage 2 //
+
+if (randomact == 1) {
+    randomscene = Math.floor(Math.random()*5+1)
+    if (randomscene == 1) {
+        randomlinestart = 1
+        randomlineend = 41;
+    }
+    if (randomscene == 2) {
+        randomlinestart = Math.floor(Math.random()*24+1)
+        randomlineend = randomlinestart + 40;
+    }
+    if (randomscene == 3) {
+        randomlinestart = Math.floor(Math.random()*74+1)
+        randomlineend = randomlinestart + 40;
+    }
+    if (randomscene == 4) {
+        randomlinestart = 1
+        randomlineend = 41;
+    }
+    if (randomscene == 5) {
+        randomlinestart = Math.floor(Math.random()*226+1)
+        randomlineend = randomlinestart + 40;
+    }
+     };
+ 
+ // Act 2//
+ 
+ if (randomact2 == 2){
+    randomscene2 = Math.floor(Math.random()*5+1);
+    if (randomscene2 == 1) {
+        randomlinestart2 = 1
+        randomlineend2 = 36;
+    }
+    if (randomscene2 == 2) {
+        randomlinestart2 = 1
+        randomlineend2 = 38;
+    }
+    if (randomscene2 == 3) {
+        randomlinestart2 = Math.floor(Math.random()*120+1)
+        randomlineend2 = randomlinestart2 + 40;
+    }
+    if (randomscene2 == 4) {
+        randomlinestart2 = Math.floor(Math.random()*80+1)
+        randomlineend2 = randomlinestart2 + 40;
+    }
+    if (randomscene2 == 5) {
+        randomlinestart2 = Math.floor(Math.random()*131+1)
+        randomlineend2 = randomlinestart2 + 40;
+    }
+ };
+ 
+ // Act 3//
+ 
+ if (randomact2 == 3) {
+    randomscene2 = Math.floor(Math.random()*4+1);
+    if (randomscene2 == 1) {
+        randomlinestart2 = Math.floor(Math.random()*109+1)
+        randomlineend2 = randomlinestart2 + 40;
+    }
+    if (randomscene2 == 2) {
+        randomlinestart2 = Math.floor(Math.random()*26+1)
+        randomlineend2 = randomlinestart2 + 40;
+    }
+    if (randomscene2 == 3) {
+        randomlinestart2 = 1
+        randomlineend2 = 50;
+    }
+    if (randomscene2 == 4) {
+        randomlinestart2 = Math.floor(Math.random()*304+1)
+        randomlineend2 = randomlinestart2 + 40;
+    }
+    
+ };
+ 
+ // Act 4//
+ 
+ if (randomact2 == 4) {
+    randomscene2 = Math.floor(Math.random()*3+1);
+    if (randomscene2 == 1) {
+        randomlinestart2 = 1
+        randomlineend2 = 59;
+    }
+    if (randomscene2 == 2) {
+        randomlinestart2 = Math.floor(Math.random()*73+1)
+        randomlineend2 = randomlinestart2 + 40;
+    }
+    if (randomscene2 == 3) {
+        randomlinestart2 = 1
+        randomlineend2 = 35;
+    }
+ };  
+ 
+ // Act 5//
+ 
+ if (randomact2 == 5) {
+    randomscene2 = 1
+    randomlinestart2 = Math.floor(Math.random()*345+1)
+    randomlineend2 = randomlinestart2 + 40;
+ 
+ 
+ };
+
+//  passage 3 //
+
+if (randomact3 == 1) {
+    randomscene3 = Math.floor(Math.random()*5+1)
+    if (randomscene3 == 1) {
+        randomlinestart3 = 1
+        randomlineend3 = 41;
+    }
+    if (randomscene == 2) {
+        randomlinestart3 = Math.floor(Math.random()*24+1)
+        randomlineend3 = randomlinestart3 + 40;
+    }
+    if (randomscene3 == 3) {
+        randomlinestart3 = Math.floor(Math.random()*74+1)
+        randomlineend3 = randomlinestart3 + 40;
+    }
+    if (randomscene3 == 4) {
+        randomlinestart3 = 1
+        randomlineend3 = 41;
+    }
+    if (randomscene3 == 5) {
+        randomlinestart3 = Math.floor(Math.random()*226+1)
+        randomlineend3 = randomlinestart3 + 40;
+    }
+     };
+ 
+ // Act 2//
+ 
+ if (randomact3 == 2){
+    randomscene3 = Math.floor(Math.random()*5+1);
+    if (randomscene3 == 1) {
+        randomlinestart3 = 1
+        randomlineend3 = 36;
+    }
+    if (randomscene3 == 2) {
+        randomlinestart3 = 1
+        randomlineend3 = 38;
+    }
+    if (randomscene3 == 3) {
+        randomlinestart3 = Math.floor(Math.random()*120+1)
+        randomlineend3 = randomlinestart3 + 40;
+    }
+    if (randomscene3 == 4) {
+        randomlinestart3 = Math.floor(Math.random()*80+1)
+        randomlineend3 = randomlinestart3 + 40;
+    }
+    if (randomscene3 == 5) {
+        randomlinestart3 = Math.floor(Math.random()*131+1)
+        randomlineend3 = randomlinestart3 + 40;
+    }
+ };
+ 
+ // Act 3//
+ 
+ if (randomact3 == 3) {
+    randomscene3 = Math.floor(Math.random()*4+1);
+    if (randomscene3 == 1) {
+        randomlinestart3 = Math.floor(Math.random()*109+1)
+        randomlineend3 = randomlinestart3 + 40;
+    }
+    if (randomscene3 == 2) {
+        randomlinestart3 = Math.floor(Math.random()*26+1)
+        randomlineend3 = randomlinestart3 + 40;
+    }
+    if (randomscene3 == 3) {
+        randomlinestart3 = 1
+        randomlineend3 = 50;
+    }
+    if (randomscene3 == 4) {
+        randomlinestart3 = Math.floor(Math.random()*304+1)
+        randomlineend3 = randomlinestart3 + 40;
+    }
+    
+ };
+ 
+ // Act 4//
+ 
+ if (randomact3 == 4) {
+    randomscene3 = Math.floor(Math.random()*3+1);
+    if (randomscene3 == 1) {
+        randomlinestart3 = 1
+        randomlineend3 = 59;
+    }
+    if (randomscene3 == 2) {
+        randomlinestart3 = Math.floor(Math.random()*73+1)
+        randomlineend3 = randomlinestart3 + 40;
+    }
+    if (randomscene3 == 3) {
+        randomlinestart3 = 1
+        randomlineend3 = 35;
+    }
+ };  
+ 
+ // Act 5//
+ 
+ if (randomact3 == 5) {
+    randomscene3 = 1
+    randomlinestart3 = Math.floor(Math.random()*345+1)
+    randomlineend3 = randomlinestart3 + 40;
+ 
+ 
+ };
+ 
+
 const passagedisplay = ' Act ' + randomact + ' Scene ' + randomscene + ' Lines ' + randomlinestart + ' to ' + randomlineend;
+const passagedisplay2 = ' Act ' + randomact2 + ' Scene ' + randomscene2 + ' Lines ' + randomlinestart2 + ' to ' + randomlineend2;
+const passagedisplay3 = ' Act ' + randomact3 + ' Scene ' + randomscene3 + ' Lines ' + randomlinestart3 + ' to ' + randomlineend3;
 
 document.getElementById('passagedisplay').innerText = passagedisplay;
+document.getElementById('passagedisplay2').innerText = passagedisplay2;
+document.getElementById('passagedisplay3').innerText = passagedisplay3;
+
+console.log(randomact, randomact2, randomact3)
 }
 document.getElementById('generate')
      .addEventListener('click', generatepassage)
